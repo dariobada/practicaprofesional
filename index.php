56e2a7dfc6ae53a81319e7f0a225b84f6211ac02
martinyoia <70449204+martinyoia@users.noreply.github.com>
2020-10-07 20:14:29 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,171 +1,156 @@
 <?php
+	
+	if(count($_POST) > 0){
 
 	include 'datosConexionBase.inc';
 
-	//establezco conexión con la base de datos
+	 //establezco conexión con la base de datos
 	$dbconn = pg_connect("host=" . HOST . " port=" . PORT . " dbname=" . BASE . " user=" . USUARIO . " password=" . PASS);
 
-
-	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	//-------------- ACA HARDCODEO EL ID DE USUARIO ----------------
-	//$_POST['usuario'] = 'pepe';
-	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-	$cons = "'" . 'pepe' . "'";
-
-	$parte1 = 'SELECT "nro_cuenta" , "tipo_cuenta", "cod_moneda", "saldo"
-  				FROM public."CUENTAS" 
-			   WHERE "id_cuenta" IN (SELECT "id_cuenta"
-					     FROM public."CUENTAS_USUARIOS"
-					    WHERE "id_usuario" in (SELECT "id_usuario"
-											     FROM public."USUARIOS"
-											    WHERE "nombre_usuario" = ';
-
-	$parte2 = '))';
-
+	//validar el usuario 
+	$cons = "'" . $_POST['usuario'] . "'";
+	$parte1 = 'SELECT * FROM public."USUARIOS" WHERE "nombre_usuarios" = ';
 	
-
 	//realizo la consulta
-	$resultado = pg_query($dbconn, $parte1 . $cons . $parte2);
+	$resultado = pg_query($dbconn, $parte1 . $cons);
 
+	 if (!$resultado) {
+		echo "Se produjo un error\n";
+		exit;
+	} else{
 
-	if (!$resultado) {
-			echo "Ocurrió un error.\n";
-			exit;
+		if (pg_num_rows($resultado) == 1){
+
+			$fila = pg_fetch_assoc($resultado);
+
+			if (!$fila['pass'] == sha1($_POST['pass'])){
+
+				echo "Usuario o contraseña incorrecta";
+				exit();
+			}
+
+			if (!$fila['cod_estado'] == 'A'){
+
+				echo "El usuario no se encuentra activo";
+				exit();
+
+			}
+
+			echo "Usuario correcto";
+
+		}else{
+
+			echo "Usuario o contraseña incorrecta 2";
+
+		}
+
+		//echo '<script language="javascript">';
+		//echo 'alert("GRACIAS POR TU PREGUNTA! SI TENES OTRA NO DUDES EN ENVIARLA.")';
+		//echo '</script>';
 	}
 
-	//pg_close($dbconn);
+
+	 pg_close($dbconn);
+	}
 
 
 ?>
 
 
+
+
 <!DOCTYPE html>
+
+<style>
+	 html, body{
+            height:100%;
+            width:100%;
+            margin:0%;
+        }
+        
+
+        div.general{
+
+        	
+        	font-size: 25px;
+        	text-align: center;
+        	box-sizing: border-box;
+        	padding-top: 5%;
+            margin: auto;
+            height: 50%;           
+            width:50%;
+            background: linear-gradient(0deg, darkblue, darkcyan); 
+           
+        }
+
+        img.logo{
+
+			width: 20%;
+			margin: center;
+			padding-top: 2%;
+			padding-left: 1%;
+		}
+
+		button{
+              
+                width: 130px;
+                height: 60px;          
+                margin: auto;
+            }
+        input{
+        	
+                width: 300px;
+                height: 20px;          
+                margin: auto;
+
+        }
+    
+</style>
 
 <html>
 
 
 
 	<head>
-		<title>Consulta de saldos</title>
+		<title>Iniciar sesión</title>
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
 
-		<style>
-
-			html, body{
-				height:100%;
-				width:100%;
-				margin:0%;
-				
-			}
-
-			div.contenedorPrincipal{
-				height:100%;
-				width:100%;
-				background-color:red;
-			}
-
-			div.menuSuperior{
-				width:100%;
-				height:15%;
-				float:left;
-
-			}	
 		
-			div.headerOpcionImpar{
-				background-color:darkblue;
-				height:100%;
-				width:20%;
-				float:left;
-				color:white;
-				font-size:25px;
-				font-weight:bold;
-				justify-content:center;
-				align-items:center;
-				display:flex;
-			}	
-
-			div.headerOpcionPar{
-				background-color:lightblue;
-				width:20%;
-				height:100%;
-				float:left;
-				color:white;
-				font-size:25px;
-				font-weight:bold;
-				justify-content:center;
-				align-items:center;
-				display:flex;
-			}
-
-			div.contenedorBody{
-				height:100%;
-				background-color:salmon;
-
-
-
-
-			}
-
-			div.interiorBody{
-				background-color:white;
-				width:50%;
-				height:50%;
-				left:25%;
-				position:absolute;
-				top:35%;
-
-
-			}
-			
-		</style>
 	</head>
 
-	<body>
-		<div class="contenedorPrincipal">
-			<div class="menuSuperior">
-				<div class="headerOpcionImpar">Logo</div>
-				<div class="headerOpcionPar">Consulta de saldos</div>
-				<div class="headerOpcionImpar">Tarjetas</div>
-				<div class="headerOpcionPar">Transferencias</div>
-				<div class="headerOpcionImpar">Cerrar sesión</div>
-			</div>
-			<div class="contenedorBody">
-				<div class="interiorBody">
-					
-					<?php
-
-						if (pg_num_rows($resultado) == 0){
-							echo "No posee cuentas";
-						} else{
-							
-							echo '<table>';
-							echo '<tr>';
-							echo '<th>Número cuenta</th>';
-							echo '<th>Tipo</th>';
-							echo '<th>Moneda</th>';
-							echo '<th>Saldo</th>';
-							echo '</tr>';
-							while ($fila = pg_fetch_assoc($resultado)) {
-								echo '<tr>';
-  								echo '<td>' . $fila['nro_cuenta'] . '</td>';
-  								echo '<td>' . $fila['tipo_cuenta'] . '</td>';
-  								echo '<td>' . $fila['cod_moneda'] . '</td>';
-  								echo '<td>' . $fila['saldo'] . '</td>';
-  								echo '</tr>';
-							}
-							echo '</table>';
 
 
-						}
+	<body background="fondo.jpg">
 
-						
-					?>
+		<img src="logo.png" class="logo">
 
-				</div>
-			</div>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+
+
+		<div class="general">
+			<form action="" method="post">
+				<label>Usuario</label>
+				<br>
+				<input type="text" name="usuario" id="usuario" autofocus="autofocus" required="required" title="Ingrese su usuario">
+				<br>
+				<br>
+				<label>Contraseña</label>
+				<br>
+				<input type="password" name="pass" id="pass" required="required" title="Ingrese su contraseña">
+				<br>
+				<br>
+
+				<button type="submit">Iniciar sesión</button>
+			</form>
 		</div>
+		
 	
 				
 	</body>
